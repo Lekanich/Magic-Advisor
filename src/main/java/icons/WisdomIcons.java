12feadf1de3a5dc3378fa0dab65748da4c5d6ad5
lekanich <lekan1992@gmail.com>
2020-04-04 11:16:29 +0300

    - added toolbar icon - added plugin icon

--- src/main/java/icons/WisdomIcons.java
+++ src/main/java/icons/WisdomIcons.java
@@ -6,4 +6,5 @@ import javax.swing.*;
 
 public class WisdomIcons {
 	public static final Icon MAIN = IconLoader.getIcon("/icons/task.png");
+	public static final Icon BALL = IconLoader.getIcon("/icons/ball_toolbar.svg");
 }

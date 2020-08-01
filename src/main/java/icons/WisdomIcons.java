4185e79664c52b97fe2aaee5e7c717e710d6198b
lekanich <lekan1992@gmail.com>
2020-08-01 10:25:47 +0300

    * add philosophy tool icon

--- src/main/java/icons/WisdomIcons.java
+++ src/main/java/icons/WisdomIcons.java
@@ -5,6 +5,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class WisdomIcons {
-	public static final Icon MAIN = IconLoader.getIcon("/icons/task.png");
+	public static final Icon PHILOSOPHY = IconLoader.getIcon("/icons/phi_icon.svg");
 	public static final Icon BALL = IconLoader.getIcon("/icons/ball_toolbar.svg");
 }

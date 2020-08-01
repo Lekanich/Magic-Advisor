cbd8662fc22d0517443cd680d9dcbb8f0d4f73fc
lekanich <lekan1992@gmail.com>
2020-08-01 09:23:51 +0300

    * split bundles

---
+++ src/main/java/lekanich/messages/MagicBallBundle.java
@@ -0,0 +1,30 @@
+package lekanich.messages;
+
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import com.intellij.AbstractBundle;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.PropertyKey;
+
+/**
+ * @author Lekanich
+ */
+@NoArgsConstructor
+public class MagicBallBundle {
+    @NonNls
+    private static final String BUNDLE_NAME = "messages.MagicBallBundle";
+
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    public static String message(@PropertyKey(resourceBundle = BUNDLE_NAME) String key, Object... params) {
+        return AbstractBundle.message(BUNDLE, key, params);
+    }
+
+    public static List<String> getAdvices() {
+        return BUNDLE.keySet().stream()
+                .map(MagicBallBundle::message)
+                .collect(Collectors.toList());
+    }
+}

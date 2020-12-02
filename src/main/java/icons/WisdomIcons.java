626bdc4c7c808f3ff0c4085b27c72235c1382b6d
lekanich <lekan1992@gmail.com>
2020-12-02 20:45:23 +0200

    * use not deprecated api for icons

--- src/main/java/icons/WisdomIcons.java
+++ src/main/java/icons/WisdomIcons.java
@@ -5,6 +5,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class WisdomIcons {
-	public static final Icon PHILOSOPHY = IconLoader.getIcon("/icons/phi_icon.svg");
-	public static final Icon BALL = IconLoader.getIcon("/icons/ball_toolbar.svg");
+	public static final Icon PHILOSOPHY = IconLoader.getIcon("/icons/phi_icon.svg", WisdomIcons.class);
+	public static final Icon BALL = IconLoader.getIcon("/icons/ball_toolbar.svg", WisdomIcons.class);
 }

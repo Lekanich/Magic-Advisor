d943d12d3828d2b5b8a86c7245ea708ce5b3dd67
lekanich <lekan1992@gmail.com>
2021-02-17 18:44:34 +0200

    * remain only master message bundle

--- src/main/java/lekanich/NotificationCenter.java
+++ src/main/java/lekanich/NotificationCenter.java
@@ -3,7 +3,6 @@ package lekanich;
 import java.util.Optional;
 import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
-import lekanich.messages.CommonBundle;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -12,8 +11,9 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationCenter {
+
     public static NotificationGroup getNotificationGroup() {
-        String displayId = CommonBundle.message("wisdom.main.name");
+        String displayId = "Magic Advisor";
         return Optional.ofNullable(NotificationGroup.findRegisteredGroup(displayId))
                 .orElseGet(() -> new NotificationGroup(displayId, NotificationDisplayType.BALLOON, true));
     }
